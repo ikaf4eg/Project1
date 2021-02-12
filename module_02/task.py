42a98d9bde039b48ef04038111e5e7c74df6ab19
ikaf4eg <ikaf4eg@yandex.ru>
2021-02-13 01:06:00 +0300

    module_02_v2

---
+++ module_02/task.py
@@ -0,0 +1,15 @@
+print('Введите пароль:')
+str1 = input()
+str2 = 'Ваш пароль состоит только из цифр'
+try:
+    1/len(str1)
+    int(str1)
+except ZeroDivisionError:
+    str2 = 'Вы ввели пустой пароль'
+except ValueError:
+    str2 = 'Требования к паролю соблюдены'
+except:
+    str2 = 'Видимо что-то случилось'
+
+print(str2)
+    

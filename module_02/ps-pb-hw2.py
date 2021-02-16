66edf33235890ef42edcc7456538f094b502e5e0
ikaf4eg <ikaf4eg@yandex.ru>
2021-02-17 00:21:16 +0300

    homework2 v4

--- module_02/ps-pb-hw2.py
+++ module_02/ps-pb-hw2.py
@@ -3,7 +3,7 @@ import os
 
 os.system('cls') # Очищаем консоль
 
-i = 0 # переменная для цикла
+i = 0 # Здесь и далее i - переменная для цикла
 
 # Создаём словари с паролями для юзеров
 account1 = {'login': 'ivan','password':'q1'}
@@ -12,10 +12,10 @@ account3 = {'login': 'olga','password':'q3'}
 account4 = {'login': 'anna','password':'q4'}
 
 # Создаём бд с данными юзеров
-user1 = {'name':'Иван','age':'20','account':account1}
-user2 = {'name':'Пётр','age':'25','account':account2}
-user3 = {'name':'Ольга','age':'18','account':account3}
-user4 = {'name':'Анна','age':'27','account':account4}
+user1 = {'name':'Иван','age':20,'account':account1}
+user2 = {'name':'Пётр','age':25,'account':account2}
+user3 = {'name':'Ольга','age':18,'account':account3}
+user4 = {'name':'Анна','age':27,'account':account4}
 
 user_list = [user1,user2,user3,user4] # Список всех юзеров
 key = input('Введите ключ (name или account): ') # Запрашиваем ключ
@@ -26,11 +26,11 @@ try:
     # Вывод информации по ключу обо всех пользователях
     while i < 4:
         user_list0 = user_list[i]
-        key_print = user_list0[f'{key}']
-        print (f'значение ключа {key} для юзера {i+1} = ' + key_print)
+        key_print = user_list0[f'{str(key)}']
+        print (f'значение ключа {key} для юзера {i+1} = ' + str(key_print))
         i = i + 1
 # Вывод ошибки и выход из программы
-except:
+except KeyError:
     print("Введенный ключ не найден")
     exit()
 
@@ -39,7 +39,8 @@ print(' ') # Разделение блоков программы
 # Запрашиваем порядковый номер юзера с проверкой
 try:
     listnumb = input('Введите порядковый номер: ') 
-    user_listx = user_list[int(listnumb) - 1]
+    # Номера юзеров 1-4, для работы со списком вычитаем 1
+    user_listx = user_list[int(listnumb) - 1] 
     # Вывод ошибки и выход из программы
 except:
     print('Пользователь с указанным номером не найден')
@@ -48,7 +49,7 @@ except:
 # Вывод данных по выбранному юзеру
 print('Данные по юзеру №' + listnumb + ':')
 print('имя: ' + user_listx['name'])
-print('возраст: ' + user_listx['age'])
+print('возраст: ' + str(user_listx['age']))
 print('логин: ' + user_listx['account']['login'])
 print('пароль: ' + user_listx['account']['password'])
 
@@ -58,7 +59,7 @@ average_age = 0
 
 while a < 4: # Цикл для расчета среднего возраста юзеров
     user_list1 = user_list[a]
-    average_age = average_age + int(user_list1['age'])
+    average_age = average_age + user_list1['age']
     a = a + 1
 
 # Вывод среднего возраста юзеров из БД
@@ -67,25 +68,28 @@ print('Средний возраст пользователей:' + str(average_
 print(' ') # Разделение блоков программы
 
 # Запрашиваем порядковый номер юзера для переноса в конец списка
-Y = input ('Введите номер пользователя, которого нужно переместить в конец: ')
-print(user_list[int(Y)]) # Вывод юзера для переноса в конец списка
+move_user = input ('Введите номер пользователя, которого нужно переместить в конец: ')
+
+# Вывод юзера для переноса в конец списка
+# Номера юзеров 1-4, для работы со списком вычитаем 1
+print(user_list[int(move_user)-1]) 
 print(' ')
 
 # Вывод списка юзеров до изменения
 i = 0
 while i < 4:
-    user_listY = user_list[i]
-    print (user_listY)
+    user_list_mu = user_list[i]
+    print (user_list_mu)
     i = i + 1
 
 # Перенос юзера в конец списка
 print(' ')
-user_listY = user_list.pop(int(Y))
-user_list.append(user_listY)
+user_list_mu = user_list.pop(int(move_user)-1)
+user_list.append(user_list_mu)
 
 # Вывод списка юзеров после изменения
 i = 0
 while i < 4:
-    user_listY = user_list[i]
-    print (user_listY)
+    user_list_mu = user_list[i]
+    print (user_list_mu)
     i = i + 1

9f306f4489f957bfa9eaa2a3fc956c35a5eb0372
ikaf4eg <ikaf4eg@yandex.ru>
2021-03-24 00:32:12 +0300

    module_05 post_get

--- module_05/onlineshop/app/views.py
+++ module_05/onlineshop/app/views.py
@@ -1,19 +1,39 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 
 @app.route('/')
 def index():
+    return render_template('index.html')
 
-    # Выполняем расчеты на сервере
-    result1 = 10 + 25
-    result2 = 200 - 300
 
-    # Создаем словарь, в который помещаем результаты расчетов
-    # Этот словарь будет являться контекстом - его передадим в функцию render_template
-    context = {
-        'result1': result1,
-        'result2': result2,
-    }
+@app.route('/create_product', methods=['POST', 'GET'])
+def create_product():
 
-    return render_template('index.html', **context)
+    context = None
+
+    if request.method == 'POST':
+        
+        # Пришел запрос с методом POST (пользователь нажал на кнопку 'Добавить товар')
+        # Получаем название товара - это значение поля input с атрибутом name="title"
+        product_title = request.form['title']
+
+        # Получаем цену товара - это значение поля input с атрибутом name="price"
+        product_price = request.form['price']
+
+        # Заполняем словарь контекста
+        context = {
+            'method': 'POST',
+            'title': product_title,
+            'price': product_price,
+        }
+    
+    elif request.method == 'GET':
+
+        # Пришел запрос с методом GET - пользователь просто открыл в браузере страницу по адресу http://127.0.0.1:5000/create_product
+        # В этом случае просто передаем в контекст имя метода
+        context = {
+            'method': 'GET',
+        }
+
+    return render_template('create_product.html', **context)

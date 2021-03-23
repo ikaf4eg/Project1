9a4dfdfaf8e06e8bb4c3fa5ecb2886cd815f0bef
ikaf4eg <ikaf4eg@yandex.ru>
2021-03-24 01:24:35 +0300

    module_05 sqlite

--- module_05/onlineshop/app/__init__.py
+++ module_05/onlineshop/app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
+# Подключаем базу данных к приложению
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../onlineshop.sqlite"
+
+# Отключаем вывод технических сообщений
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Создаем саму базу данных - объект db
+db = SQLAlchemy(app)
+
 
 from app import views
